a0a53c51f7e57f6d3bb46b193525bddc9ce4c972
Sherlock <tomekwator1997@gmail.com>
2020-02-20 18:31:02 +0100

    Witaj świecie

--- HashCodeBB_Stage1/Program.cs
+++ HashCodeBB_Stage1/Program.cs
@@ -6,7 +6,7 @@ namespace HashCodeBB_Stage1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Witaj świecie!");
         }
     }
 }
