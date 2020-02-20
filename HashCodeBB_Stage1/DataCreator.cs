07ee860ea4d31e29712a8df2c9788080e5db4c7e
Marcin filapek <filap4@gmail.com>
2020-02-20 18:56:22 +0100

    all

---
+++ HashCodeBB_Stage1/DataCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashCodeBB_Stage1
+{
+    class DataCreator
+    {
+        public DataCreator()
+        {
+        }
+
+        public static string CreateString(List<int> order)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append($"{order.Count}\n");
+            for (int i = 0; i < order.Count; i++)
+            {
+                if (i == order.Count - 1)
+                    builder.Append($"{order[i].PizzaId}\n");
+                else
+                    builder.Append($"{order[i].PizzaId} ");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ HashCodeBB_Stage1/File.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HashCodeBB_Stage1
+{
+    class File
+    {
+        public File() { }
+
+        public string[] ReadFile(string path)
+        {
+            if (!System.IO.File.Exists(path))
+                return null;
+
+            using (StreamReader stream = new StreamReader(path))
+            {
+                string all = stream.ReadToEnd();
+                string[] result = all.Split(new string[] { " ", "\n" }, StringSplitOptions.None);
+                return result;
+            }
+        }
+
+        public void CreateFile(string path, string data)
+        {
+            using (FileStream fs = System.IO.File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(data);
+                fs.Write(info, 0, info.Length);
+            }
+        }
+    }
+}
